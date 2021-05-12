2e82c1fce4342ec549ef4fcd45574db72550ef7f
ropalopoli <palopoli.romina@gmail.com>
2021-05-12 14:19:48 -0300

    Clase 2

--- miproyecto.py
+++ miproyecto.py
@@ -1,22 +1,20 @@
-print ("Bienvenido")
-#edad = 21
-edad = input("Digame su edad")
-#print (edad)
-edad = int(edad)
-#if edad >= 18:
-     #print("Usted es mayor de edad")
-     #if edad == 65:
-         #print ("Haga el trámite de jubilación")
-#else:
-    #print("Usted es menor de edad")
+clientes = int(input("Ingrese número de clientes: "))
+for i in range(clientes):
+    print (i)
+    print ("Bienvenido")
+    edad = input("Digame su edad")
+    edad = int(edad)
 
-if edad<0:
-    print ("usted no nacio")
-elif edad <18:
-    print ("Usted es menor de edad")
-elif edad <65:
-    print ("usted es mayor de edad")
-elif edad <120:
-    print ("usted es jubilado")
+    if edad<0:
+        print ("usted no nacio")
+    elif edad <18:
+        print ("Usted es menor de edad")
+    elif edad <65:
+        print ("usted es mayor de edad")
+    elif edad <120:
+        print ("usted es jubilado")
+    else:
+        print ("usted esta en el otro mundo")
+        break
 else:
-    print ("usted esta en el otro mundo")
+    print ("Proceso de carga finalizado")
