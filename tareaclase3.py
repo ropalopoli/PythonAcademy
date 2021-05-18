a00ba7b86ea3cbda769a74ae722cfbca9f6dd98a
ropalopoli <palopoli.romina@gmail.com>
2021-05-18 13:26:30 -0300

    Tarea clase 3 correccion 1

--- tareaclase3.py
+++ tareaclase3.py
@@ -8,20 +8,17 @@
 #5.1 Preguntarle si quiere viajar a Federación, Cataratas o Santa Teresita
 #Mostrar por pantalla los datos de la persona y el producto seleccionado
 def jugueteria(edad):
-    if edad <18:
-        electronico = input("Desea un juguete electrónico? Si/No: ")
-        color = input("Digame el color que desea ")
-        camina = input("Desea que camine solo? Si/No: ")
+    electronico = input("Desea un juguete electrónico? Si/No: ")
+    color = input("Digame el color que desea ")
+    camina = input("Desea que camine solo? Si/No: ")
     print("Su selección es: "+str(electronico)+" "+str(color)+" "+str(camina))
 
 def tienda_ropa(edad):
-    if edad <=64:
-        ropa = input("Desea camisa o pantanlon ? ")
+    ropa = input("Desea camisa o pantanlon ? ")
     print("Su seleccion es: "+str(ropa))
 
 def pasajes(edad):
-    if edad <120:
-        pasajes = input("Desea viajar a Federación, Cataratas o Santa Teresita? ")
+    pasajes = input("Desea viajar a Federación, Cataratas o Santa Teresita? ")
     print("Su seleccion es: "+str(pasajes))
 
 clientes = int(input("Ingrese número de clientes: "))
